4830ea25bd5ef4ad4a8dca879a2dd4c544f3cc91
murdok5 <mike.smith@puppet.com>
2016-08-11 10:23:50 -0700

    updates

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,23 @@
 class powershell5 (
-  $path = "C:\\temp\\",
+  $path = "C:/temp/",
   $installer = "Win8\.1AndW2K12R2\-KB3134758\-x64\.msu",
 ) {
 
-  file { "${path}${installer}":
+  file { "ps_installer":
     ensure => 'present',
     source => "puppet:///powershell5/${installer}",
+    path   => "${path}${installer}",
   }
+
+  file { "c:\\temp\\":
+    ensure => 'folder',
+    before => File['ps_installer'],
+  }
+
 
   service { 'windows update':
     ensure  => 'running',
-    enabled => 'true',
+    enable => 'true',
   }
 
 }
