82c2497cbde8d05580274121e253c9292cf08123
murdok5 <mike.smith@puppet.com>
2016-08-11 10:39:58 -0700

    updates

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,20 +3,26 @@ class powershell5 (
   $installer = "Win8.1AndW2K12R2-KB3134758-x64.msu",
 ) {
 
+   file { "${path}":
+    ensure => 'directory',
+    before => File['ps_installer'],
+  }
+  
   file { "ps_installer":
     ensure => 'present',
     source => "puppet:///modules/powershell5/${installer}",
     path   => "${path}${installer}",
   }
 
-  file { "${path}":
-    ensure => 'directory',
-    before => File['ps_installer'],
-  }
-
   service { 'wuauserv':
     ensure  => 'running',
     enable => 'true',
+  }
+
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU':
+    ensure   => present,
+    type   => dword,
+    data => "NoWindowsUpdate",
   }
 
 }
